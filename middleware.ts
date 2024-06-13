4c742c35080817566e0426d67ba99d1eea8022ee
Taha Shahid <tahashahid203@gmail.com>
2024-06-14 03:22:19 +0500

    Add API Webhook

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,6 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)","/api/webhook","/question/:id", "/tags", "/tags/:id", "/profile/:id", "/community", "/jobs"],
+  ignoredRoutes: ['/api/webhook', '/api/chatgpt']
 };

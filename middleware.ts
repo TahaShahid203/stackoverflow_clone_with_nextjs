1744161f22ba14d519e9c8c3e1707ea305a6c749
Taha Shahid <tahashahid203@gmail.com>
2024-06-14 05:46:23 +0500

    Changed the webhooks file name

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,5 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)","/api/webhook","/question/:id", "/tags", "/tags/:id", "/profile/:id", "/community", "/jobs"],
-  ignoredRoutes: ['/api/webhook', '/api/chatgpt']
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
 };

a726742ad6f69ea6075748027492eb85733555be
Taha Shahid <tahashahid203@gmail.com>
2024-06-17 22:32:53 -0400

    Implemented the community page and the tags page but the tags page has bugs in it for now

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,7 +2,7 @@
 
 import { connectToDatabase } from "../mongoose"
 import User from '@/database/user.model';
-import { CreateUserParams, DeleteUserParams, UpdateUserParams } from "./shared.types";
+import { CreateUserParams, DeleteUserParams, GetAllUsersParams, UpdateUserParams } from "./shared.types";
 import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
 
@@ -75,5 +75,21 @@ export async function deleteUser(params: DeleteUserParams){
     } catch (error) {
         console.log(error);
         throw error
+    }
+}
+
+export async function getAllUsers(params: GetAllUsersParams){
+    try {
+        connectToDatabase();
+
+        // const {page = 1, pageSize = 20, filter, searchQuery} = params;
+
+        const users = await User.find({}).sort({createdAt: -1})
+
+        return {users};
+        
+    } catch (error) {
+        console.log(error);
+        throw error;
     }
 }

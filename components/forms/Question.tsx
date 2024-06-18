6425faee3be1882bf464d7d37932e47ddf078a7b
Taha Shahid <tahashahid203@gmail.com>
2024-06-18 02:20:12 -0400

    Implemented the question detail page along with the Answer model. Reused the TinyMC form for the Answer form

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -20,6 +20,7 @@ import { Input } from "@/components/ui/input";
 import { Badge } from "../ui/badge";
 import Image from "next/image";
 import { createQuestion } from "@/lib/actions/question.action";
+import { useTheme } from "@/context/ThemeProvider";
 
 const type: any = "create";
 
@@ -28,6 +29,7 @@ interface Props {
 }
 
 const Question = ({ mongoUserId }: Props) => {
+  const {mode}=useTheme()
   const editorRef = useRef(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
 
@@ -174,6 +176,8 @@ const Question = ({ mongoUserId }: Props) => {
                       "codesample | bold italic forecolor | alignleft aligncenter |" +
                       "alignright alignjustify | bullist numlist",
                     content_style: "body { font-family:Inter; font-size:16px }",
+                    skin: mode === "dark" ? "oxide-dark" : "oxide",
+                    content_css: mode === "dark" ? "dark" : "light",
                   }}
                 />
               </FormControl>
